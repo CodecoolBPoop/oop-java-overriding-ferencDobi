1903920c085d77ff9c78daa2347b25fc5ad06ac7
ferencDobi <ferencdbi@gmail.com>
2018-08-11 19:07:25 +0200

    some tests

--- src/com/codecool/uml/overriding/Webshop.java
+++ src/com/codecool/uml/overriding/Webshop.java
@@ -3,6 +3,17 @@ package com.codecool.uml.overriding;
 public class Webshop {
 
     public static void main(String[] args) {
-	// write your code here
+        Order order = new Order();
+        Order order2 = new Order();
+        CheckoutProcess cp = new CheckoutProcess();
+        PaymentProcess pp = new PaymentProcess();
+        System.out.println(order.getStatus());
+        System.out.println(order2.getStatus());
+        cp.action(order);
+        System.out.println(order.getStatus());
+        System.out.println(order2.getStatus());
+        pp.action(order);
+        System.out.println(order.getStatus());
+        System.out.println(order2.getStatus());
     }
 }
